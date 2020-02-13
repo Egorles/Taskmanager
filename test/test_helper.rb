6ee7005d04fc6b5bb0bbd2982a4da37c85bf985c
Egor <egor.lenivin@gmail.com>
2020-02-13 13:00:31 +0300

    test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
-ENV['RAILS_ENV'] ||= 'test'
-require 'simplecov'
-SimpleCov.start
 require 'coveralls'
 Coveralls.wear!
+require 'simplecov'
+SimpleCov.start
+
+ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
 require 'rails/test_help'
 
