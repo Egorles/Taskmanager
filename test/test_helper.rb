e265db5bdf52550ac101b1050b2fa75b8a3be70a
Egor <egor.lenivin@gmail.com>
2020-02-18 14:59:53 +0300

    authorization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+include AuthHelper
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -21,4 +22,19 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   include FactoryBot::Syntax::Methods
+end
+
+module SignInHelper
+  def sign_in_as(admin)
+    post session_path, params: {
+      session: {
+        password: admin.password,
+        email: admin.email
+      }
+    }
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
 end
