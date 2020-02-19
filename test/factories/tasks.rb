069f4fdf9ca5acdad968629b2b72b82e209ad0b9
Egor <egor.lenivin@gmail.com>
2020-02-19 15:10:06 +0300

    api

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author { nil }
-    assignee { nil }
-    expired_at
+    author_id { nil }
+    assignee_id { nil }
+    expired_at { nil }
   end
 end
+
